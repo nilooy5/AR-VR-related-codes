fc7268a95f0505df68f6193e8bd877e9e0719d70
Fazal Mahmud Niloy <niloooy@gmail.com>
2022-11-01 01:34:03 +1100

    init files added 2

---
+++ Week 3.r
@@ -0,0 +1,53 @@
+#***********************************************************************
+#   AR/VR for Data Analysis and Communication
+#      
+#   Week 3 - Take home exercise
+#
+#   Dr Raul Fernandez-Rojas
+#***********************************************************************
+
+# clear global environment
+rm(list=ls())
+
+# Clear plots
+if(!is.null(dev.list())) dev.off()
+
+#clean Console  as command (CTRL + L)
+cat("\014") 
+
+
+# load dataset
+data("iris")
+# explore dataset
+View(iris)
+
+# create vectors 
+x <- iris$Sepal.Length
+y <- iris$Petal.Length
+z <- iris$Sepal.Width
+
+
+#************************ shiny package **********************
+# load package
+library(shiny)
+
+ui <- fluidPage(
+        titlePanel("My Take Home Shiny App!"),
+        sidebarLayout(
+                sidebarPanel(
+                        selectInput("dataSet", 
+                                    "Select the mumeric variable to plot:",
+                                    choices = colnames(iris[1:4]))),
+                mainPanel(plotOutput("plotOutput"))
+        )
+)
+
+server <- function(input, output){
+        output$plotOutput<-renderPlot({
+                hist(iris[ ,input$dataSet],
+                     main =paste("Histogram of", input$dataSet),
+                     xlab=input$dataSet)
+        })
+}
+
+shinyApp(ui = ui, server = server)
