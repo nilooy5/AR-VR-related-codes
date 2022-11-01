8bc7870f583e63a0673aa1a0dac8d74fff5ac8d1
Fazal Mahmud Niloy <niloooy@gmail.com>
2022-11-01 19:12:49 +1100

    temp fix for q5

--- Q5.r
+++ Q5.r
@@ -22,20 +22,33 @@ a_scatterplot <- function(json_data, x, y, z, ...) {
 # end of function
 # convert the dataset into a JSON file
 work_data <- iris
+# work_data
+# change names of columns Petal.Width, Sepal.Width, Petal.Length to x, y, z
+names(work_data)[names(work_data) == "Petal.Width"] <- "x"
+names(work_data)[names(work_data) == "Sepal.Width"] <- "y"
+names(work_data)[names(work_data) == "Petal.Length"] <- "z"
+work_data$size <- 0.2
+work_data$color <- "#ff0000"
 # conver species to factor
 # if work_data$Species is setosa, versicolor or virginica, then it will be 1, 2 or 3
 work_data$Species <- as.factor(work_data$Species)
-# conver factor to numeric
+# convert factor to numeric
 work_data$Species <- as.numeric(work_data$Species)
+# if species is 1, 2, 3 then color is #ff0000, #00ff00, #4CC3D9
+work_data$color[work_data$Species == 1] <- "#ff0000"
+work_data$color[work_data$Species == 2] <- "#00ff00"
+work_data$color[work_data$Species == 3] <- "#4CC3D9"
+work_data
+
 
 iris_json <- jsonlite::toJSON(work_data)
 # create scene
 my_scene <- a_scene(.template = "empty",
                     .children = list(
                       a_scatterplot(iris_json, # dataset
-                                    x = "Petal.Width",
-                                    y = "Sepal.Width",
-                                    z = "Petal.Length", # choose columns in your dataset
+                                    x = "x",
+                                    y = "y",
+                                    z = "z", # choose columns in your dataset
                                     val = "Species", # colour scale
                                     xlabel = "Petal.Length", ylabel = "Petal.Width", zlabel = "Sepal.Width", #axis labels
                                     showFloor = TRUE,
